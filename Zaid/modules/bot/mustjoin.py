adbcbc5d8dd3a8322c3025861d7fc9d72641191c
HKMUSIC <himanshu4hk9@gmail.com>
2025-07-25 10:51:18 +0530

    Update mustjoin.py

--- Zaid/modules/bot/mustjoin.py
+++ Zaid/modules/bot/mustjoin.py
@@ -5,7 +5,7 @@ from Zaid import app
 
 #--------------------------
 
-MUST_JOIN = "BeastSupportGc"
+MUST_JOIN = "BMW_USERBOT_II"
 #------------------------
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
